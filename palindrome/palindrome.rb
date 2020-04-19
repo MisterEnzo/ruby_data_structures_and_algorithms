4a151baf5923d905b2bf284e69a4d11c8711bccc
enz <enzo_naguiat@yahoo.com>
2020-04-19 23:16:51 +0900

    add instructions to the problem set and comment out the solution

--- palindrome/palindrome.rb
+++ palindrome/palindrome.rb
@@ -1,8 +1,23 @@
-def palindrome(str)
-  reverse_string = str.split('').reverse.join
-  if reverse_string == str
-    true
-  else
-    false
-  end
-end
+# Create a method called 'palindrome' that takes a str and checks if its a palindrome. 
+# It should return 'true' if the string is a palindrome, and 'false' otherwise.
+# Palindrome - reversing the characters of the string will result in the same string. ex('ABBA')
+
+
+
+
+
+
+
+
+
+
+# Solution
+
+# def palindrome(str)
+#   reverse_string = str.split('').reverse.join
+#   if reverse_string == str
+#     true
+#   else
+#     false
+#   end
+# end
