75cc59a5ec8cc2d2f8dfe817ea56cd1bd9d8fa27
enz <enzo_naguiat@yahoo.com>
2020-04-27 00:01:50 +0900

    comment out solution

--- anagrams/anagrams.rb
+++ anagrams/anagrams.rb
@@ -7,30 +7,43 @@ require 'byebug'
 # anagrams("blaBla", "lablab") ---> false (it is case sensitive)
 # anagrams("tashi", "ashit") ---> true
 
-def char_map(str)
-  map = {}
-  chars = str.split('')
-  for c in chars
-    if map[c]
-      map[c] += 1
-    else
-      map[c] =1
-    end
-  end
-  map
-end
 
 
 
-def anagrams(str1, str2)
-  anagram = true
-  map1 = char_map(str1)
-  map2 = char_map(str2)
-  map1.each do |k, v|
-    if !map2[k] || map2[k] != v
-      anagram = false
-    end
-  end
-  anagram
 
-end
+
+
+
+
+
+
+
+
+
+# solution
+
+# def char_map(str)
+#   map = {}
+#   chars = str.split('')
+#   for c in chars
+#     if map[c]
+#       map[c] += 1
+#     else
+#       map[c] =1
+#     end
+#   end
+#   map
+# end
+
+# def anagrams(str1, str2)
+#   anagram = true
+#   map1 = char_map(str1)
+#   map2 = char_map(str2)
+#   map1.each do |k, v|
+#     if !map2[k] || map2[k] != v
+#       anagram = false
+#     end
+#   end
+#   anagram
+
+# end
